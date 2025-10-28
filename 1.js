68d2020dcfa7553459d7bda9804bf0989aed286e
kitty <Kitty@kitty.com>
2025-10-28 16:06:46 +0800

    show-to-TH

--- 1.js
+++ 1.js
@@ -5,58 +5,30 @@
 // [15, 28, 4, 2, 43] 印出 6
 // [23, 71, 33, 82, 1] 印出 24
 
-//需要寫一小段文字說明為什麼這樣做
-
-// function sumOfSmallestValues(arr) {
-//   // 實作程式碼寫在這裡
-// }
-
-// const list1 = [19, 5, 42, 2, 77];
-// const list2 = [23, 15, 59, 4, 17];
-
-// console.log(sumOfSmallestValues(list1)); // 印出 7
-// console.log(sumOfSmallestValues(list2)); // 印出 19
-
-///
-///
-///
-///
-///
-
-// const minVal = Math.min(...list1);
-// console.log(minVal);
-// //找出陣列最小值 的函數
-
-const list1 = [17, 15, 16, 16, 16, -1, 16, 0, 0, 2, 5.5, 77.77, 3, 17];
-
 function getMin(A) {
   // 找出陣列最小值 的函數
-  let chiisai = A[0];
+  let chiisai = A[0]; //假設A[0]是最小值
   for (let i = 0; i < A.length; i++) {
     if (A[i] < chiisai) {
+      //用迴圈逐一比較 若你比較小 你來當最小值
       chiisai = A[i];
     }
   }
-  return chiisai;
+  return chiisai; //回傳最小值
 }
 
-const m = getMin(list1); //  list1的最小值
+////////////////////////////
 
-const newA = list1.filter((numb) => numb !== m); // 去除最小值的陣列
+function sumOfSmallestValues(arr) {
+  const m = getMin(arr); //  陣列的最小值
+  const newA = arr.filter((numb) => numb !== m); // 去除最小值的陣列
+  const lessM = getMin(newA); // 陣列的第二小值 (【去除最小值的陣列】的最小值)
 
-console.log(newA);
+  return m + lessM;
+}
 
-// function getIndex(A, numb) {
-//   // 找出最小值的index的函數
-//   let doko;
-//   for (let j = 0; j < A.length; j++) {
-//     if (A[j] == numb) {
-//       doko = j;
-//       return doko;
-//     }
-//   }
-// }
+const list1 = [19, 5, 42, 2, 77];
+const list2 = [23, 15, 59, 4, 17];
 
-// const koko = getIndex(list1, m); // list1的最小值的位置
-
-// const newA = list1.splice(koko, 1); // 移除list1的最小值的新陣列
+console.log(sumOfSmallestValues(list1)); // 印出 7
+console.log(sumOfSmallestValues(list2)); // 印出 19
