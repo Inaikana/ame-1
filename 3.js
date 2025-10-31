944008e81be54f219d1e79190cbe68f4888e5505
kitty <Kitty@kitty.com>
2025-10-31 10:53:43 +0800

    3-OK

--- 3.js
+++ 3.js
@@ -2,26 +2,21 @@
 // 程式語言：JavaScript
 // 題目：完成函數的內容，把陣列裡的 0 都移到最後面
 
-// let list = [false, 1, 0, -1, 2, 0, 1, 3, "a"];
-let arr = [false, 1, 0, -1, 2, 0, 1, 3, "a", 0, 5];
-
-// function moveZerosToEnd(arr) {
-//   // 程式碼寫在這裡
-// }
-
-// let result = moveZerosToEnd(list);
-// console.log(result); // 印出 [false, 1, -1, 2, 1, 3, "a", 0, 0]
-
-let ikutsu = 0;
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] == 0) {
-    arr.splice(i, 1);
-    ikutsu++;
+let list = [false, 1, 0, -1, 2, 0, 1, 3, "a"];
+function moveZerosToEnd(arr) {
+  let ikutsu = 0; // 記錄有幾個0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      // 【陷阱】要用=== 不然 0 == false
+      arr.splice(i, 1); // 每當陣列元素是0時 去除它
+      ikutsu++; // 0的數量+1
+    }
   }
+  for (let j = 1; j <= ikutsu; j++) {
+    // 依照 ikutsu 記錄 用迴圈把0 push回陣列最後面
+    arr.push(0);
+  }
+  return arr;
 }
-
-for (let j = 1; j < ikutsu; j++) {
-  arr.push(0);
-}
-
-console.log(arr);
+let result = moveZerosToEnd(list);
+console.log(result); // 印出 [false, 1, -1, 2, 1, 3, "a", 0, 0]
