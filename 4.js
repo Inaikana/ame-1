dfaf96ef5a1c83babe7513009fb5d6df3741b67f
Inai <aromr990426@gmail.com>
2025-11-01 20:24:41 +0800

    4-改善補零的方法

--- 4.js
+++ 4.js
@@ -9,21 +9,10 @@ function humanReadableTimer(seconds) {
   let hr = Math.floor(min / 60); // 有幾小時
   let nokoruMin = min % 60; // 剩幾分(除不盡的)
 
-  if (nokoruSec < 10) {
-    //設定秒 < 10會用0開頭
-    nokoruSec = `0${nokoruSec}`;
-  }
-
-  if (nokoruMin < 10) {
-    //設定分 < 10會用0開頭
-    nokoruMin = `0${nokoruMin}`;
-  }
-
-  if (hr < 10) {
-    //設定小時 < 10會用0開頭
-    hr = `0${hr}`;
-  }
-  return `${hr}:${nokoruMin}:${nokoruSec}`;
+  return ` ${String(hr).padStart(2, "0")}:${String(nokoruMin).padStart(
+    2,
+    "0"
+  )}:${String(nokoruSec).padStart(2, "0")}`;
 }
 
 console.log(humanReadableTimer(0)); // 印出 00:00:00
@@ -36,25 +25,3 @@ console.log(humanReadableTimer(45296)); // 印出 12:34:56
 console.log(humanReadableTimer(86399)); // 印出 23:59:59
 console.log(humanReadableTimer(86400)); // 印出 24:00:00
 console.log(humanReadableTimer(359999)); // 印出 99:59:59
-
-// Math.floor() 無條件捨去
-
-// const seconds = 0; // 改這邊測試
-// const min = Math.floor(seconds / 60); // 有幾分鐘
-// let nokoruSec = seconds % 60; // 剩幾秒
-
-// let hr = Math.floor(min / 60); // 有幾小時
-// let nokoruMin = min % 60; // 剩幾分
-
-// if (nokoruSec < 10) {
-//   nokoruSec = `0${nokoruSec}`;
-// }
-
-// if (nokoruMin < 10) {
-//   nokoruMin = `0${nokoruMin}`;
-// }
-
-// if (hr < 10) {
-//   hr = `0${hr}`;
-// }
-// console.log(`${hr}:${nokoruMin}:${nokoruSec}`);
