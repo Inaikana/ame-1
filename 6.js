83c6845b32bf67ffdc5709022f18373858676ae5
Inai <aromr990426@gmail.com>
2025-11-02 09:43:00 +0800

    6-改良

--- 6.js
+++ 6.js
@@ -3,20 +3,16 @@
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
 function findDifferent(numbers) {
-  let onaji; //設一個變數先不給值 等等用來裝陣列中相同的數字
-  if (numbers[0] !== numbers[1] && numbers[0] !== numbers[2]) {
-    onaji = numbers[1]; // 如果 numbers[0] 同時和後兩個都不一樣 那它就是不一樣的 ; 代表 numbers[1] 是一樣的
-  } else if (numbers[1] !== numbers[0] && numbers[1] !== numbers[2]) {
-    onaji = numbers[0]; // 如果 numbers[1] 同時和前後兩個都不一樣 那它就是不一樣的 ; 代表 numbers[0] 是一樣的
-  } else {
-    // 沒觸發上兩式代表 numbers[0] = numbers[1] = 一樣的
-    onaji = numbers[0];
-  }
-
-  for (let i = 0; i < numbers.length; i++) {
-    // 用迴圈找出不一樣的
-    if (numbers[i] !== onaji) {
-      return numbers[i];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    //因為我會拿numbers[i]和numbers[i + 1]相比 所以迴圈跑到 numbers.length - 1 就可以了
+    if ((numbers[i] + numbers[i + 1]) / 2 !== numbers[i]) {
+      // numbers[i]和numbers[i + 1]相加除2 若不等於numbers[i] 就代表這兩個數不一樣
+      if (numbers[i] == numbers[i - 1] || numbers[i] == numbers[i + 2]) {
+        // 拿numbers[i]去跟前一個元素或後兩個元素比(用「或」避免i是最左或最右時) 找出numbers[i]、numbers[i+1]哪個才是不一樣的
+        return numbers[i + 1];
+      } else {
+        return numbers[i];
+      }
     }
   }
 }
@@ -24,19 +20,3 @@ function findDifferent(numbers) {
 console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1])); // 印出 3
 console.log(findDifferent([2, 2, 2, 4, 2, 2])); // 印出 4
 console.log(findDifferent([8, 3, 3, 3, 3, 3, 3, 3])); // 印出 8
-
-// const numbers = [1, 1, 1, 1, 1, 7, 1, 1, 1];
-// let onaji;
-// if (numbers[0] !== numbers[1] && numbers[0] !== numbers[2]) {
-//   onaji = numbers[1];
-// } else if (numbers[1] !== numbers[0] && numbers[1] !== numbers[2]) {
-//   onaji = numbers[0];
-// } else {
-//   onaji = numbers[0];
-// }
-
-// for (let i = 0; i < numbers.length; i++) {
-//   if (numbers[i] !== onaji) {
-//     console.log(numbers[i]);
-//   }
-// }
