1d74ed5088b5262b2993782a1da85b5281bcbc4b
kitty <Kitty@kitty.com>
2025-11-05 11:37:21 +0800

    10準備用reduce搭起來

--- 10.js
+++ 10.js
@@ -3,12 +3,36 @@
 // 題目：把數字以 10 進位展開式呈現，數字均為大於 0 的正整數
 // 範例：9527 變成 "1000 x 9 + 100 x 5 + 10 x 2 + 7"
 
-function expandedForm(num) {
-  // 實作寫在這裡
+function tens(n) {
+  // 10的n次方函數
+  let jyuu = 1; // 10的0次方為1  所以初始值為1
+  for (let i = 0; i < n; i++) {
+    jyuu *= 10;
+  }
+  return jyuu;
 }
 
-console.log(expandedForm(8)); // 印出 8
-console.log(expandedForm(25)); // 印出 10 x 2 + 5
-console.log(expandedForm(148)); // 印出 100 x 1 + 10 x 4 + 8
-console.log(expandedForm(1450)); // 印出 1000 x 1 + 100 x 4 + 10 x 5
-console.log(expandedForm(60308)); // 印出 10000 x 6 + 100 x 3 + 8
+// function expandedForm(num) {
+//   // 實作寫在這裡
+// }
+
+// console.log(expandedForm(8)); // 印出 8
+// console.log(expandedForm(25)); // 印出 10 x 2 + 5
+// console.log(expandedForm(148)); // 印出 100 x 1 + 10 x 4 + 8
+// console.log(expandedForm(1450)); // 印出 1000 x 1 + 100 x 4 + 10 x 5
+// console.log(expandedForm(60308)); // 印出 10000 x 6 + 100 x 3 + 8
+
+const num = 60308;
+const moji = num.toString(); // 把數字轉成文字
+const ikutsu = moji.length; // 這串數字有幾個字 = 它是幾位數
+const hitotsu = [...moji]; // 數字 逐一的陣列
+
+for (
+  let j = ikutsu - 1;
+  j >= 0;
+  j-- // ikutsu = 幾位數 = 幾個數字 但陣列是從0開始 所以要-1
+) {
+  console.log(tens(j));
+}
+
+// console.log(tens(0));
