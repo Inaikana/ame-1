70dd2f1a8443c14a14d1e4dd0773512b8ce6d304
kitty <Kitty@kitty.com>
2025-11-14 11:20:13 +0800

    12只能兩輪11個9有bug

--- 12.js
+++ 12.js
@@ -4,11 +4,28 @@
 // 範例：9527 => 9 + 5 + 2 + 7 => 23 => 2 + 3 => 5
 // 1450 => 1 + 4 + 5 + 0 => 10 => 1 + 0 => 1
 
+function add(n) {
+  // 把數字一個一個加起來的函數
+  const one = [...n.toString()]; // 逐一顯示數字的陣列
+  mix = one.reduce((last, element) => {
+    return Number(last) + Number(element);
+  });
+  return mix;
+}
+
 const numberReducer = (num) => {
-  // 實作寫在這裡
+  let thisRound = add(num); // 做一次數字一個一個加起來
+
+  if (thisRound >= 10) {
+    // 如果結果不是一位數
+    thisRound = add(thisRound); //
+  }
+
+  return thisRound;
 };
 
-console.log(numberReducer(9527)); // 印出 5
-console.log(numberReducer(1450)); // 印出 1
-console.log(numberReducer(5566108)); // 印出 4
-console.log(numberReducer(1234567890)); // 印出 9
+// console.log(numberReducer(9527)); // 印出 5
+// console.log(numberReducer(1450)); // 印出 1
+// console.log(numberReducer(5566108)); // 印出 4
+// console.log(numberReducer(1234567890)); // 印出 9
+console.log(numberReducer(99999999999));
