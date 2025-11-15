b0763386849fa6bc0f3e6477ca125d7d8913fc66
Inai <aromr990426@gmail.com>
2025-11-15 22:07:14 +0800

    15OK

--- 15.js
+++ 15.js
@@ -6,7 +6,20 @@
 // "abcdefg" -> ['ab', 'cd', 'ef', 'g_']
 
 function splitString(str) {
-  // 實作寫在這裡
+  const one = [...str]; // 轉成一個個文字的陣列
+  let two = []; // 預先準備空陣列
+  if (one.length == 0) {
+    return one; // 若是空字串 就回傳空陣列
+  } else if (one.length % 2 != 0) {
+    one.push("_"); // 若是奇數字串 就補"_"
+  }
+
+  for (let i = 0; i < one.length - 1; i = i + 2) {
+    // 一次抓兩個做字串相加 丟入空陣列
+    // 用one.length - 1   避免最後一個元素還執行
+    two.push(one[i] + one[i + 1]);
+  }
+  return two;
 }
 
 console.log(splitString("abcdef")); // ["ab", "cd", "ef"]
