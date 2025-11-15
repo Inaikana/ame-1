dbb22ca65ae82c5774346d51afb8410836f1c51d
Inai <aromr990426@gmail.com>
2025-11-15 22:35:04 +0800

    16 變大寫OK

--- 16.js
+++ 16.js
@@ -3,10 +3,21 @@
 // 題目：把原本 snake_case 的字轉換成 camelCase 格式
 // 範例："hello_world" -> "helloWorld"
 
-function toCamelCase(str) {
-  // 實作寫在這裡
-}
+// function toCamelCase(str) {
+//   // 實作寫在這裡
+// }
 
-console.log(toCamelCase("book")); // book
-console.log(toCamelCase("book_store")); // bookStore
-console.log(toCamelCase("get_good_score")); // getGoodScore
+// console.log(toCamelCase("book")); // book
+// console.log(toCamelCase("book_store")); // bookStore
+// console.log(toCamelCase("get_good_score")); // getGoodScore
+
+const str = "get_good_score";
+const open = str.split("_");
+
+function big(word) {
+  const one = [...word];
+  const first = String.fromCharCode(one[0].charCodeAt(0) - 32);
+  one.splice(0, 1, first);
+  const done = one.reduce((a, b) => a + b);
+  return done;
+}
