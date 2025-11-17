6fda0c38152b6518c618572c0f2887888b7b8fde
Inai <aromr990426@gmail.com>
2025-11-17 22:55:14 +0800

    19OK

--- 19.js
+++ 19.js
@@ -3,7 +3,15 @@
 // 題目：檢查是否為某個數字的平方數
 
 function isSquare(num) {
-  // 實作寫在這裡
+  const square = Math.sqrt(num); // 先取平方 若是負數就是NaN
+  const test = square * 10;
+  if (test % 10 == 0) {
+    // 如果是整數 ×10後 再÷10還是整數
+    // NaN計算後還是NaN  NaN一定是false
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isSquare(0)); // true
