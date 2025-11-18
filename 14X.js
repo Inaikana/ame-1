6a598e3955681115933da57bb520248f50fb4ff4
Inai <aromr990426@gmail.com>
2025-11-18 23:07:28 +0800

    14大爆炸

--- 14X.js
+++ 14X.js
@@ -13,17 +13,17 @@ function uniqueOrder(sequence) {
     arr = sequence;
   }
 
-  const word = arr.map((n) => n.toString()); // 把陣列內容變成全部都是文字
-  const count = word.reduce((obj, key) => {
-    obj[key] = (obj[key] || 0) + 1;
-    return obj;
-  }, {});
-
-  for (const key in count) {
-    if (key.charCodeAt(0) > 64) {
-      asw.push(key);
-    } else {
-      asw.push(Number(key));
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (arr[i - 1] != arr[i] && arr[i] != arr[i + 1]) {
+      asw.push(arr[i - 1]);
+      asw.push(arr[i]);
+      asw.push(arr[i + 1]);
+      const next = i + 1;
+      i = next;
+    } else if (arr[0] != arr[1]) {
+      asw.push(arr[0]);
+    } else if (arr[i] != arr[i + 1]) {
+      asw.push(arr[i + 1]);
     }
   }
 
@@ -34,3 +34,19 @@ console.log(uniqueOrder("AABCC")); // [ 'A', 'B', 'C']
 console.log(uniqueOrder("AAABBBCCBCC")); // [ 'A', 'B', 'C', 'B', 'C']
 console.log(uniqueOrder([1, 2, 1, 2, 1])); // [ 1, 2, 1, 2, 1 ]
 console.log(uniqueOrder([1, 1, 1, 2, 2, 2, 1])); // [1, 2, 1]
+
+// const word = arr.map((n) => n.toString()); // 把陣列內容變成全部都是文字
+// const count = word.reduce((obj, key) => {
+//   obj[key] = (obj[key] || 0) + 1;
+//   return obj;
+// }, {});
+
+// for (const key in count) {
+//   if (key.charCodeAt(0) > 64) {
+//     asw.push(key);
+//   } else {
+//     asw.push(Number(key));
+//   }
+// }
+
+// return asw;
