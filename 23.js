bbe374c1d0ca899abc3a7be2f29da01c0a2ab479
kitty <Kitty@kitty.com>
2025-11-20 12:28:00 +0800

    23 OK

--- 23.js
+++ 23.js
@@ -3,7 +3,33 @@
 // 題目：算出 N 個數字的最大公因數
 
 function calcGCD(...numbers) {
-  // 實作程式碼寫在這裡
+  const min = Math.min(...numbers); // 找出陣列min
+  if (numbers.includes(0)) {
+    return "0沒有任何因數"; // 防呆 0沒有任何因數
+  } else {
+    let clear = 0; // 計算用
+    for (let j = min; j > 0; j--) {
+      // 從陣列中的min開始 每個數都拿來跟整個陣列相除
+      // 例:[50,62,17]  min=17
+      // 第一輪 50÷17 62÷17 17÷17
+      // 第二輪 50÷16 62÷16 17÷16
+      // ...直到做完1為止
+
+      for (let i = 0; i < numbers.length; i++) {
+        // 陣列中每個元素都要跟這輪由min遞減的值相除
+        if (numbers[i] % j == 0) {
+          clear++;
+          // 陣列中若能整除 clear就+1
+        }
+      }
+      if (clear == numbers.length) {
+        return j;
+        // 如果這輪j = 陣列長度 代表 全都能整除 印出j 即為最大公因數
+        // 如果上面用 console.log()  這邊要加 break; 停止迴圈
+      }
+      clear = 0; // 每輪j做完要清空clear
+    }
+  }
 }
 
 console.log(calcGCD(10)); // 10
