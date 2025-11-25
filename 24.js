8f4803bc7d805148a2c7de50fa5f122136b56eca
Inai <aromr990426@gmail.com>
2025-11-25 23:07:16 +0800

    24 OK

--- 24.js
+++ 24.js
@@ -5,75 +5,73 @@
 
 import calcGCD from "./23.js";
 
-// function calcLCM(...numbers) {
-//   // 實作程式碼寫在這裡
-// }
+function Xtwo(n1, n2) {
+  // 可以取得兩個數的最小公倍數
+  const ans = (n1 * n2) / calcGCD(n1, n2);
+  return ans;
+}
 
-// console.log(calcLCM(10)); // 10
-// console.log(calcLCM(103, 27)); // 2781
-// console.log(calcLCM(21, 15, 18)); // 630
-// console.log(calcLCM(104, 96, 36, 88)); // 41184
+function calcLCM(...numbers) {
+  if (numbers.length < 2) {
+    return numbers[0];
+  } else {
+    const LCM = numbers.reduce((r, n) => {
+      return Xtwo(r, n);
+    });
 
-function findKid(num) {
-  // 找出num的所有因數(1省略)
-  let kid = [];
-  for (let i = 2; i <= num; i++) {
-    if (num % i == 0) {
-      kid.push(i);
-    }
+    return LCM;
   }
-  return kid;
 }
 
-function onlyKid(num) {
-  //  找出num的所有質因數
-  return findKid(num).filter((kid) => {
-    if (findKid(kid).length == 1) {
-      return true;
-    }
-  });
-}
+console.log(calcLCM(10)); // 10
+console.log(calcLCM(103, 27)); // 2781
+console.log(calcLCM(21, 15, 18)); // 630
+console.log(calcLCM(104, 96, 36, 88)); // 41184
 
-function howMany(num, y) {
-  // 找出 num 有幾個 y 因數
-  // 例 625 = 5^4 > 得到4
-  let start = num;
-  let n;
-  for (n = 0; start >= y; n++) {
-    if (start % y == 0) {
-      start = start / y;
-    } else {
-      break;
-    }
-  }
-  return n;
-}
-
-function useOver(num, y) {
-  // 找完 num 有幾個 y 因數後 除完剩下的值
-  let start = num;
-  for (let n = 0; start >= y; n++) {
-    if (start % y == 0) {
-      start = start / y;
-    } else {
-      break;
-    }
-  }
-  return start;
-}
-
-console.log(howMany(625, 5));
-console.log(useOver(625, 5));
-
-// const numbers = [104, 96, 36, 88];
-// const min = Math.min(...numbers);
-// let cell = [];
-// let cellNum;
-// for (let x = 2; x <= min; x++) {
-//   for (let i = 0; i < numbers.length; i++) {
-//     if (numbers[i] % x == 0) {
-//       cell.push(x);
-//       continue;
+// function findKid(num) {
+//   // 找出num的所有因數(1省略)
+//   let kid = [];
+//   for (let i = 2; i <= num; i++) {
+//     if (num % i == 0) {
+//       kid.push(i);
 //     }
 //   }
+//   return kid;
+// }
+
+// function onlyKid(num) {
+//   //  找出num的所有質因數
+//   return findKid(num).filter((kid) => {
+//     if (findKid(kid).length == 1) {
+//       return true;
+//     }
+//   });
+// }
+
+// function howMany(num, y) {
+//   // 找出 num 有幾個 y 因數
+//   // 例 625 = 5^4 > 得到4
+//   let start = num;
+//   let n;
+//   for (n = 0; start >= y; n++) {
+//     if (start % y == 0) {
+//       start = start / y;
+//     } else {
+//       break;
+//     }
+//   }
+//   return n;
+// }
+
+// function useOver(num, y) {
+//   // 找完 num 有幾個 y 因數後 除完剩下的值
+//   let start = num;
+//   for (let n = 0; start >= y; n++) {
+//     if (start % y == 0) {
+//       start = start / y;
+//     } else {
+//       break;
+//     }
+//   }
+//   return start;
 // }
