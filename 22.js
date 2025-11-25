cda9657e2cd9bca6aa75f330ceb6ff19b3b21ecd
kitty <Kitty@kitty.com>
2025-11-25 17:01:42 +0800

    22 OK

--- 22.js
+++ 22.js
@@ -6,19 +6,23 @@ class Queue {
   constructor() {
     this.callStack = []; // 建立一個空陣列
     this.length = this.callStack.length; // 建立 length 仿陣列 能查看長度
-    this.takeout = 0;
   }
   enqueue(goin) {
+    // 製作push功能 把輸入的值依序放入陣列
     if (goin) {
+      // 有東西才放
       this.callStack.push(goin);
-      this.length = this.callStack.length;
+      this.length = this.callStack.length; // 放完要即時更新陣列長度
     }
   }
   dequeue() {
-    this.takeout = this.callStack[0];
-    this.callStack.splice(0, 1);
-    this.length = this.callStack.length;
-    return this.takeout;
+    const takeout = this.callStack[0];
+    // 先記錄這次丟了什麼
+    // 如果用return this.callStack[0] 會回傳【丟完後的】
+    // 且 return 一定要放最後
+    this.callStack.splice(0, 1); // 清除陣列第一個元素
+    this.length = this.callStack.length; // 丟完要即時更新陣列長度
+    return takeout; // 回傳這次丟了什麼
   }
 }
 
