a089c7d343d7d3888cc4a8ee13aa84915049b8e2
Inai <aromr990426@gmail.com>
2025-11-25 22:17:07 +0800

    21 OK

--- 21.js
+++ 21.js
@@ -3,7 +3,23 @@
 // 題目：實作 Stack 資料結構
 
 class Stack {
-  // 實作寫在這裡
+  constructor() {
+    this.callStack = []; // 建立一個空陣列
+    this.size = 0; // 建立size 用來查陣列長度
+  }
+  push(ele) {
+    if (ele) {
+      // 有輸入值才做
+      this.callStack.push(ele); // 把輸入值丟入陣列
+      this.size = this.callStack.length; // 加完要即時更新陣列長度
+    }
+  }
+  pop() {
+    const rm = this.callStack[this.callStack.length - 1]; // 用rm紀錄丟了什麼
+    this.callStack.splice(-1, 1); // 移除陣列最後一個
+    this.size = this.callStack.length; // 丟完要即時更新陣列長度
+    return rm; // 回傳丟了什麼
+  }
 }
 
 const stack = new Stack();
